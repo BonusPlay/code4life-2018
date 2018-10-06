ed8cae20c5a604a52f4318a45b5f06cf857a8b39
BonusPlay <root@bonusplay.pl>
2018-10-06 23:07:03 +0200

    one file

--- src/components/QFinal.vue
+++ src/components/QFinal.vue
@@ -1,10 +1,18 @@
 <template>
-	
+	<div>
+		Your score is {{calc_score}}/20.
+	</div>
 </template>
 
 <script>
 	export default {
-		name: "QFinal"
+		name: "QFinal",
+		computed: {
+			calc_score: function() {
+				const answers = this.$store.getters.questions.map(question => question.calculateScore());
+				return answers.filter(true).length;
+			}
+		}
 	}
 </script>
 
