ddb9b4331db2b6e9c7700c0716345241233542f5
Marek <krysztofikmarek@gmail.com>
2018-10-07 12:49:12 +0200

    as

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,13 +1,25 @@
 <template>
 	<div>
+
+
 		<v-img :src="splash">
-			<v-layout class="hidden-sm-and-down" align-start justify-center column fill-height>
-				<v-flex md-3 offset-md1>
-					<v-btn to="/questionnaire/start" :large=true color="success" style="margin-top:150px;">
+			<div style="height: 75px; background-color: #4d4d4b;"></div>
+			<v-img :src="logo"
+				   height="120"
+				   width="120"
+				   style="margin-left: 20%; top: -15%;"/>
+			<v-layout class="hidden-sm-and-down" align-start justify-center column >
+
+				<v-flex md-3 offset-md1 style="background-color: aqua;">
+
+					<p  style="float: left; font-size: 1.5em;" >Test wczesnego wykrycia autyzmu</p>
+					<br>
+					<v-btn to="/questionnaire/start" :large=true color="success" style="float: left;">
 						Wykonaj Darmowy Test
 						<v-icon>arrow_forward</v-icon>
 					</v-btn>
 				</v-flex>
+
 			</v-layout>
 		</v-img>
 		<v-container>
@@ -17,7 +29,11 @@
 				</v-flex>
 				<v-flex s12 md6 class="text-md-center">
 					<p class="display-3">Test wczesnego wykrycia autyzmu</p>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+					et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+					aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+					culpa qui officia deserunt mollit anim id est laborum.
 					<br>
 					<v-btn to="/questionnaire/start" :large=true color="success">
 						Wykonaj Darmowy Test
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,18 +1,13 @@
 <template>
-	<div>
-		<Toolbar/>
 		<v-content>
 			<router-view/>
 		</v-content>
-	</div>
 </template>
 
 <script>
-	import Toolbar from './Toolbar';
 
 	export default {
 		name: "Main",
-		components: {Toolbar}
 	}
 </script>
 
