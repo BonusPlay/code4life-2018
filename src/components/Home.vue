fffb690c2b598453306f4d1c741eaa9cbeb31f9a
Marek <krysztofikmarek@gmail.com>
2018-10-07 14:44:07 +0200

    as

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -6,6 +6,7 @@
 				   height="120"
 				   width="120"
 				   style="margin-left: 20%; top: -15%;"/>
+
 			<v-layout class="hidden-sm-and-down" align-start justify-center column>
 
 				<v-flex md-3 offset-md1
@@ -21,6 +22,8 @@
 
 			</v-layout>
 		</v-img>
+
+
 		<v-flex sm12 lg8 offset-lg2>
 			<v-container>
 				<v-layout>
@@ -29,8 +32,12 @@
 					</v-flex>
 					<v-flex s12 md6 class="text-md-center">
 						<p class="display-3">O TEŚCIE</p>
-						<p>Każde dziecko rozwija się inaczej, ma inne potrzeby i inaczej odbiera świat. Jednak pewne sygnały, które rodzice obserwują u swoich dzieci mogą sugerować poważne problemy.</p>
-						<p>Sprawdź czy Twoje dziecko może mieć spektrum autyzmu. Rozwiąż krótki test, który zweryfikuje Twoje przypuszczenia i podpowie czy zachowania Twojego malucha są odpowiednie dla jego wieku i etapu rozwoju. Pamiętaj, że badanie nie jest pełną diagnozą, a jedynie wskazówką czy powinieneś skonsultować się ze specjalistą.</p>
+						<p>Każde dziecko rozwija się inaczej, ma inne potrzeby i inaczej odbiera świat. Jednak pewne
+							sygnały, które rodzice obserwują u swoich dzieci mogą sugerować poważne problemy.</p>
+						<p>Sprawdź czy Twoje dziecko może mieć spektrum autyzmu. Rozwiąż krótki test, który zweryfikuje
+							Twoje przypuszczenia i podpowie czy zachowania Twojego malucha są odpowiednie dla jego wieku
+							i etapu rozwoju. Pamiętaj, że badanie nie jest pełną diagnozą, a jedynie wskazówką czy
+							powinieneś skonsultować się ze specjalistą.</p>
 						<p>Bez względu na wynik, pamiętaj, że musisz potwierdzić informację u specjalistów.</p>
 
 						<br>
@@ -40,7 +47,23 @@
 						</v-btn>
 					</v-flex>
 				</v-layout>
-				<v-layout wrap>
+				<v-layout style="padding-top: 4em;">
+					<v-flex md6>
+						<p class="display-3">FUNDACJA JiM</p>
+						<p style="padding: 0 8%;">Fundacja JiM tworzy lepszy świat dla osób z autyzmem.
+							Opiekuje się ponad 3000 dzieci z całej Polski, zapewniając i finansując niezbędną terapię.
+							Co roku, lekarze z Kliniki JiM dieagnozują bezpłatnie kilkaset maluchów. Ponadto Fundacja
+							wspiera ponad 4000 rodziców dzieci z autyzmem z całego kraju, należących do Klubu Rodziców
+							JiM, zapewniając im wiedzę, szkolenia i pomoc prawną.</p>
+
+						<p style="padding: 0 8%;">Fundacja JiM prowadzi przedszkole i szkoły dla dzieci z autyzmem oraz wspiera osoby dorosłe.
+							Tworzy kampanie społeczne niosące wiedzę o autyznie, takie jak Polska na Niebiesko.</p>
+					</v-flex>
+					<v-flex md6>
+						<v-img :src="kid1" />
+					</v-flex>
+				</v-layout>
+				<v-layout wrap style="padding-top: 4em;">
 					<v-flex lg-5>
 						<gmap-map
 							:center="{lat:52.11444444,lng:19.42361111}"
@@ -68,16 +91,19 @@
 <script>
 	import splash from "../assets/splash.jpg";
 	import logo from "../assets/logo.png";
+	import kid1 from "../assets/kid1.jpg";
 	import kids from "../assets/kid2.jpg";
 	import GMapInfo from './GMapInfo';
 
 	export default {
 		name: "Home",
 		data() {
+
 			return {
 				splash: splash,
 				logo: logo,
 				kids: kids,
+				kid1: kid1,
 				mapZoom: 5,
 				institutes: null,
 				selected: 6
